39bc5dfc82419cb87a2fcf518fcd310c65e53066
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-07-27 06:51:42 -0400

    Inheritance and Polymorphis.  Special Methods.

---
+++ Webserver/test.py
@@ -0,0 +1,8 @@
+#!C:\Users\18043\.vscode\extensions\ms-python.vscode-pylance-2021.7.5
+
+print("Content-type:text/html\n")
+
+print("<h1>This is the Home Page</h1>")
+
+for i in range(5):
+    print("Item ") + str(i) + "<br/>"
